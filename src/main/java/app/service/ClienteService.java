590938000f423a69b0f365e01c56e0cdee204df1
Josilan <cjosilan@gmail.com>
2024-06-18 21:43:17 -0400

    Primeiro commit

---
+++ src/main/java/app/service/ClienteService.java
@@ -0,0 +1,15 @@
+package app.service;
+
+import app.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
